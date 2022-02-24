745b99d24fd3f7e172aa847147499a9069303abd
Martin Viola <violamartinalejandro@gmail.com>
2022-02-24 16:25:31 -0300

    link a iconos footer + quitar estilos a anchor en iconos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,11 +15,21 @@ function Footer() {
       <div className="ContactContainer">
         <h4>Contact us 24/7</h4>
         <div>
-          <PhoneIcon fontSize="large"/>
-          <EmailIcon fontSize="large"/>
-          <LinkedInIcon fontSize="large"/>
-          <InstagramIcon fontSize="large"/>
-          <FacebookIcon fontSize="large"/>
+          <a href="https://www.youtube.com/watch?v=Zbk2SBgPToc">
+            <PhoneIcon fontSize="large"/>
+          </a>
+          <a href="https://mail.google.com/mail/u/0/#inbox">
+            <EmailIcon fontSize="large"/>
+          </a>
+          <a href='https://www.linkedin.com/'>
+            <LinkedInIcon fontSize="large"/>
+          </a>
+          <a href="https://www.instagram.com/">
+            <InstagramIcon fontSize="large"/>
+          </a>
+          <a href="https://www.facebook.com/">
+            <FacebookIcon fontSize="large"/>
+          </a>
         </div>
       </div>
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3007.7896716113983!2d-58.3744259181317!3d-34.60898325905424!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccadb57fd1e5f%3A0xcc737c4eb0a8614b!2sPlaza%20de%20Mayo!5e0!3m2!1sen!2sar!4v1645369024367!5m2!1sen!2sar" title="map"></iframe>
