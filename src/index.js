4353cfc86c90f476fa768c5a2cb9660350ce00b8
Martin Viola <violamartinalejandro@gmail.com>
2022-03-05 18:24:46 -0300

    1st commit sprint3

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import mainReducer from './redux/reducers/mainReducer';
+import thunk from 'redux-thunk';
+
+const reduxStore = createStore(mainReducer,applyMiddleware(thunk))
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={reduxStore}>
+     <App />
+    </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/reducers/mainReducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+
+import citiesReducer from './citiesReducer';
+import authReducer from './authReducer'
+
+const mainReducer = combineReducers({
+
+    citiesReducer,
+    authReducer
+
+})
+
+export default mainReducer
